6c92c0cc6c7f432114dd60066142524226ec754b
Daniel Arrais <danielarraiscarvalho@gmail.com>
2021-06-17 17:51:58 -0300

    Sample One - Updating the Rendered Element

---
+++ 03-rendering-elements/src/components/SampleOne.js
@@ -0,0 +1,43 @@
+import { Component } from 'react';
+
+class SampleOne extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentTime: null,
+        }
+    }
+
+    componentDidMount() {
+        this.updateTime();
+        setInterval(() => this.updateTime(), 1000);
+    }
+
+    updateTime() {
+        const currentTime = this.formatedCurrentTime();
+        this.setState({
+            currentTime: currentTime
+        })
+    }
+
+    formatedCurrentTime() {
+        return new Date().toLocaleTimeString("pt-BR", {
+            hour: "numeric",
+            minute: "2-digit",
+            second: "2-digit",
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Updating the Rendered Element</h1>
+                <h1>Relógio: {this.state.currentTime}</h1>
+            </div>
+        );
+    }
+}
+
+export default SampleOne;
