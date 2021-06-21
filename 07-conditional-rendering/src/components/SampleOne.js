af0314e1d3534cd905c1116c7cef6f094cffbb09
Daniel Arrais <danielarraiscarvalho@gmail.com>
2021-06-21 13:14:54 -0300

    Sample one - Inline If-Else with Conditional Operator and Preventing Component from Rendering

--- 07-conditional-rendering/src/components/SampleOne.js
+++ 07-conditional-rendering/src/components/SampleOne.js
@@ -24,17 +24,83 @@ class ConditionalElementVariable extends Component {
 
         return (
             <div>
-                <h1>Element Variables and Inline If-Else with Conditional Operator</h1>
-                <button onClick={() => this.toggleLogging()}>{this.state.isLogged ? 'Exit' : 'Login'}</button>
+                <h1>Element Variables and Inline If-Else with Conditional Operator</h1> <hr />
+                <button onClick={() => this.toggleLogging()}>{this.state.isLogged ? 'Exit' : 'Login'}</button><br />
                 {textInfo}
             </div>
         );
     }
 }
+class ConditionalInlineIfInJSX extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLogged: false,
+        }
+    }
+
+    toggleLogging() {
+        this.setState(prevState => ({ isLogged: !prevState.isLogged }))
+    }
+
+    render() {
+        let textInfo = <span>User logged: {this.state.isLogged && 'Daniel Arrais'}</span>;
+
+        return (
+            <div>
+                <h1>Inline If-Else with Conditional Operator</h1> <hr />
+                <button onClick={() => this.toggleLogging()}>{this.state.isLogged ? 'Exit' : 'Login'}</button><br />
+                {textInfo}
+            </div>
+        );
+    }
+
+}
+
+function NotRenderContent(props) {
+    if (!props.render) return null;
+
+    return (
+        <div>
+            <h1>Simple component</h1>
+        </div>
+    );
+}
+class NotRenderContentOfElement extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            renderChildren: false,
+        }
+    }
+
+    toggleLogging() {
+        this.setState(prevState => ({ renderChildren: !prevState.renderChildren }))
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Preventing Component from Rendering</h1> <hr />
+                <button onClick={() => this.toggleLogging()}>{this.state.renderChildren ? 'Hide' : 'Show'}</button><br />
+                <NotRenderContent render={this.state.renderChildren} />
+            </div>
+        );
+    }
+
+}
 
 class SampleOne extends Component {
     render() {
-        return <ConditionalElementVariable />
+        return (
+            <div>
+                <ConditionalElementVariable />
+                <ConditionalInlineIfInJSX />
+                <NotRenderContentOfElement />
+            </div>
+        );
     }
 }
 
