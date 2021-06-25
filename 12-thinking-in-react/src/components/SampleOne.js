08a35d28c01a5421dd89c90229554bf564cc1cac
Daniel Arrais <danielarraiscarvalho@gmail.com>
2021-06-25 08:45:33 -0300

    temp

--- 12-thinking-in-react/src/components/SampleOne.js
+++ 12-thinking-in-react/src/components/SampleOne.js
@@ -1,8 +1,55 @@
 import { Component } from 'react';
 
+class FilterableProductTable extends Component {
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <ProductTable />
+            </div>
+        );
+    }
+}
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+                <form action="/">
+                    <input type="text" placeholder="Search..." /><br />
+                    <input type="checkbox">Only Show products in stock</input>
+                </form>
+            </div>
+        );
+    }
+}
+
+class ProductTable extends Component {
+    render() {
+        return (
+            <div>
+                <ProductCategoryRow />
+                <ProductRow />
+            </div>
+        );
+    }
+}
+
+class ProductCategoryRow extends Component {
+    render() {
+        return <div></div>;
+    }
+}
+
+class ProductRow extends Component {
+    render() {
+        return <div></div>;
+    }
+}
+
 class SampleOne extends Component {
     render() {
-        return null;
+        return <FilterableProductTable />;
     }
 }
 
